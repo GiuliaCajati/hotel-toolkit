88e970aa94bad2343456b272e2157ffce6f2a8c3
giuliacajati <giuliacajati@gmail.com>
2020-11-25 13:40:42 -0500

    task crud features completed

--- app/controllers/date_events_controller.rb
+++ app/controllers/date_events_controller.rb
@@ -2,7 +2,6 @@ class DateEventsController < ApplicationController
     # skip_before_action :verify_authenticity_token
 
     def create
-       
         @date_event = DateEvent.create(date_info_id: params[:date_info_id], event_id: params[:event_id], arrivals: params[:arrivals], in_house: params[:in_house], departures: params[:departures])
         byebug
         render json: @date_event.to_json(include: [ :date_info, :event]) 
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,4 +14,16 @@ class TasksController < ApplicationController
         @task = Task.create(department_id: params[:department_id], event_id: params[:event_id], certificate: params[:certificate], project: params[:project],guest_follow_up: params[:guest_follow_up], details: params[:details])
         render json: @task.to_json(include: [:event, :department, :team_member]) 
     end 
+
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+       # debugger
+    end
+
+    def update 
+        @task = Task.find(params[:id])
+        @task.update(department_id: params[:department_id], event_id: params[:event_id], certificate: params[:certificate], project: params[:project],guest_follow_up: params[:guest_follow_up], details: params[:details])
+        render json: @task.to_json(include: [:event, :department, :team_member]) 
+    end 
 end
