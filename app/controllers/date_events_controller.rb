14f9a5d067e90ff1c886da143c4b87bebd731429
giuliacajati <giuliacajati@gmail.com>
2020-11-28 15:35:43 -0500

    date_info  includes adjusted

--- app/controllers/date_events_controller.rb
+++ app/controllers/date_events_controller.rb
@@ -3,7 +3,6 @@ class DateEventsController < ApplicationController
 
     def create
         @date_event = DateEvent.create(date_info_id: params[:date_info_id], event_id: params[:event_id], arrivals: params[:arrivals], in_house: params[:in_house], departures: params[:departures])
-        byebug
         render json: @date_event.to_json(include: [ :date_info, :event]) 
     end 
 
--- app/controllers/date_infos_controller.rb
+++ app/controllers/date_infos_controller.rb
@@ -2,19 +2,12 @@ class DateInfosController < ApplicationController
     
     def index 
         @dateinfos = DateInfo.all
-        render json: @dateinfos.to_json(include: {date_events: {}, tasks: {include: [:department]}, events: {include: [:vips, :tasks]}, vip_dates: {} }) 
+        render json: @dateinfos.to_json(include: {date_events: {}, tasks: {include: [:department]}, events: {include: {vips: {}, tasks:{include: [:department]} }}, vip_dates: {} }) 
     end 
 
     def show 
         @dateinfo = DateInfo.find(params[:id])
-        render json: @dateinfo.to_json(include: {date_events: {}, tasks: {include: [:department]}, events: {include: [:vips, :tasks]}, vip_dates: {} }) 
+        render json: @dateinfo.to_json(include: {date_events: {}, tasks: {include: [:department]}, events: {include: {vips: {}, tasks:{include: [:department]} }}, vip_dates: {} }) 
     end
     
-    # def show 
-    #     @dateinfo = DateInfo.find(params[:id])
-    #     render json: @dateinfo.to_json(include: {date_events: {}, tasks: {include: [:department]}, events: {include: [:vips, :tasks]}, vip_dates: {} }) 
-    # end
-    #tasks include department 
 end 
-
-#(include: {department: {include: {tasks: {include:[:event]}}}, tasks: {}})
